134683759a2c54ea777beb9959a1514e0fde9698
parastooTaleiniya <taleiniya.sbu@gmail.com>
2020-04-04 17:17:43 +0430

    Basic Authentication

--- app.js
+++ app.js
@@ -29,8 +29,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+
+function auth(req, res, next){
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+
+    if(!authHeader){
+      var err = new Error('You are not authenticated');
+      err.status = 401;
+      res.setHeader('WWW-Authenticate' , 'Basic');
+      return next(err);
+    }
+    var auth = new Buffer(authHeader.split(' ')[1] , "base64").toString().split(':');
+    var username = auth[0];
+    var password = auth[1];
+    if(username == 'admin' && password == 'password'){
+      next();
+    }
+    else{
+      var err = new Error('You are not authenticated');
+      err.status = 401;
+      res.setHeader('WWW-Authenticate' , 'Basic');
+      return next(err);
+    }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes' , dishRouter);
