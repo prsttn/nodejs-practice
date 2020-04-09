61091ff0b79a6912d986ef4bfe1c9e634fd5f632
parastooTaleiniya <taleiniya.sbu@gmail.com>
2020-04-09 19:43:14 +0430

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -26,6 +27,14 @@ const connect = mongoose.connect(url);
 connect.then((db) => { console.log('Connecting correctly to the server')} , (err) => { console.log(err); });
 var app = express();
 
+app.all('*' , (req, res, next) => {
+  if(req.secure){
+    return next();
+  }
+  else{
+    res.redirect(307,`https://${req.hostname}:${app.get('secPort')}/${req.url}`);
+  }
+})
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -40,10 +49,8 @@ app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/imageUpload' , uploadRouter);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes' , dishRouter);
 app.use('/promotions' , promotionRouter);
 app.use('/leaders' , leaderRouter);
